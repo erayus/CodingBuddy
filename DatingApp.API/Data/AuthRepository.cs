3c667d9a04659cbb94bd6babca100b8af44b8098
Erayus <raymondhieutran@gmail.com>
2019-12-29 21:13:23 +1100

    [New] Creating Login Repository Method

--- DatingApp.API/Data/AuthRepository.cs
+++ DatingApp.API/Data/AuthRepository.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading.Tasks;
 using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Data
 {
@@ -12,19 +13,42 @@ namespace DatingApp.API.Data
             this._context = context;
 
         }
-        public Task<User> Login(string username, string password)
+        public async Task<User> Login(string username, string password)
         {
-            throw new System.NotImplementedException();
+            var user = await _context.Users.FirstOrDefaultAsync( x => x.Username == username);
+
+            if (user == null) 
+                return null;
+
+            if (!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
+                return null;
+
+            return user;
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt)){
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                for (int i = 0; i < computedHash.Length; i++ )
+                {
+                    if (computedHash[i] != passwordHash[i]) return false;
+                }
+            };
+            return true;
         }
 
         public async Task<User> Register(User user, string password)
         {
             byte[] passwordHash, passwordSalt;
+
+            // Generate Password Hash and Salt using password, reference of passwordHash and reference of passwordSalt
             CreatePasswordHash(password, out passwordHash, out passwordSalt);
 
             user.PasswordHash = passwordHash;
             user.PasswordSalt = passwordSalt;
 
+            //Add user to the Users context and save the changes
             await _context.Users.AddAsync(user);
             await _context.SaveChangesAsync();
 
