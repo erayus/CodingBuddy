04e1246cc3f312558c28801ec57226860529cb25
Erayus <raymondhieutran@gmail.com>
2020-01-03 12:19:39 +1100

    [New] Angular Global Error Handling

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -20,7 +20,7 @@ export class RegisterComponent implements OnInit {
       console.log('Registration successful!')
     }, error => {
       console.log(error);
-    })
+    });
   }
 
   cancel() {
