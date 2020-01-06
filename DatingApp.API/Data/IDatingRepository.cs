83a0f54aa3546e14f1b0820fd66e912ec7f0f2b8
Erayus <raymondhieutran@gmail.com>
2020-01-06 14:44:54 +1100

    [New] Adding Concrete Dating Repository

--- DatingApp.API/Data/IDatingRepository.cs
+++ DatingApp.API/Data/IDatingRepository.cs
@@ -10,6 +10,6 @@ namespace DatingApp.API.Data
          void Delete<T>(T entity) where T: class;
          Task<bool> SaveAll();
          Task<IEnumerable<User>> GetUsers();
-         Task<User> GetUser();
+         Task<User> GetUser(int id);
     }
 }
