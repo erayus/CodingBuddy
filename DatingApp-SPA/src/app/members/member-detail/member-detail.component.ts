67cb0d8ec2f8fdbd455efd540e031b190a27a09d
Erayus <raymondhieutran@gmail.com>
2020-01-07 13:18:25 +1100

    [New] Styling Member-Detail Component

--- DatingApp-SPA/src/app/members/member-detail/member-detail.component.ts
+++ DatingApp-SPA/src/app/members/member-detail/member-detail.component.ts
@@ -17,13 +17,14 @@ export class MemberDetailComponent implements OnInit {
               private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.loadUser()
   }
 
   loadUser() {
     this.userServ.getUser(+this.route.snapshot.params['id']).subscribe((user: User) => {
       this.user = user;
     }, error => {
-      this.alertiFy.error(error)
+      this.alertiFy.error(error);
     });
   }
 
