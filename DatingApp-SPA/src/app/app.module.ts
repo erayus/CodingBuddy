2ba799ef35ffbfadc2706f7dd18eb15da6644443
Erayus <raymondhieutran@gmail.com>
2020-01-07 23:58:34 +1100

    [New] Adding Photo Gallery

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
@@ -6,7 +6,7 @@ import { AuthService } from './_services/auth.service';
 import {BsDropdownModule, TabsModule} from 'ngx-bootstrap';
 import { RouterModule } from '@angular/router';
 import { JwtModule } from '@auth0/angular-jwt';
-
+import {NgxGalleryModule} from 'ngx-gallery';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -26,6 +26,14 @@ export function tokenGetter() {
   return localStorage.getItem('token');
 }
 
+export class CustomHammerConfig extends HammerGestureConfig {
+  overrides = {
+    pinch: {enable: false},
+    rotate: {enable: false}
+  };
+}
+
+
 @NgModule({
    declarations: [
       AppComponent,
@@ -39,12 +47,13 @@ export function tokenGetter() {
       MessagesComponent
    ],
    imports: [
- BrowserModule,
+    BrowserModule,
     HttpClientModule,
     FormsModule,
     TabsModule.forRoot(),
     BsDropdownModule.forRoot(),
     RouterModule.forRoot(appRoutes),
+    NgxGalleryModule,
     JwtModule.forRoot({
       config: {
         tokenGetter: tokenGetter,
@@ -57,7 +66,8 @@ export function tokenGetter() {
       AuthService,
       ErrorInterceptorProvider,
       MemberDetailResolver,
-      MemberListResolver
+      MemberListResolver,
+      {provide: HAMMER_GESTURE_CONFIG, useClass: CustomHammerConfig}
    ],
    bootstrap: [
       AppComponent
