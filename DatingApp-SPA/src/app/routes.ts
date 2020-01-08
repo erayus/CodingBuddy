5243271a460d1b0e1f16bede6d931ba5248e5d27
Erayus <raymondhieutran@gmail.com>
2020-01-08 21:09:43 +1100

    [New] Adding Deactivate Guard and Browser Closing Guard to MemberEdit Component

--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -1,4 +1,4 @@
-import {Routes} from '@angular/router';
+import { Routes, CanDeactivate } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
@@ -9,6 +9,7 @@ import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { MemberEditResolver } from './_resolvers/member-edit.resolver';
+import { PreventUnsavedChangesGuard } from './_guard/prevent-unsaved-changes.guard';
 
 export const appRoutes: Routes = [
   {path: '', component: HomeComponent},
@@ -22,7 +23,9 @@ export const appRoutes: Routes = [
       {path: 'members/:id', component: MemberDetailComponent,
         resolve: {user: MemberDetailResolver}},
       {path: 'member/edit', component: MemberEditComponent,
-        resolve: {user: MemberEditResolver}},
+        resolve: {user: MemberEditResolver},
+        canDeactivate: [PreventUnsavedChangesGuard]
+      },
       {path: 'messages', component: MessagesComponent},
       {path: 'lists', component: ListsComponent},
     ]
