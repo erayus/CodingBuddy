b0797341d4074b58089b8f4ff22902692eb1e7ba
Erayus <raymondhieutran@gmail.com>
2020-01-13 01:50:26 +1100

    [New] Auto adding dummy photo for new users

--- DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
+++ DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
@@ -51,6 +51,11 @@ export class PhotoEditorComponent implements OnInit {
           isMain: res.isMain
         };
         this.photos.push(photo);
+        if (photo.isMain){
+          this.userServ.changeMemberPhoto(photo.url);
+          this.authServ.currentUser.photoUrl = photo.url;
+          localStorage.setItem('user', JSON.stringify(this.authServ.currentUser));
+        }
       }
     };
 
