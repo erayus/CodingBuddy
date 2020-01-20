15ac57952f5ce8e61cd757d5aa8194b99b89b78e
Erayus <raymondhieutran@gmail.com>
2020-01-20 17:47:31 +1100

    [New] Adding Like functionality to the UI

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -66,4 +66,8 @@ export class UserService {
   deletePhoto(userId: number, photoId: number) {
     return this.http.delete(this.baseUrl + 'users/' + userId + '/photos/' + photoId );
   }
+
+  sendLike(id: number, recipientId:number) {
+    return this.http.post(this.baseUrl + 'users/' + id + '/like/' + recipientId, {});
+  }
 }
--- DatingApp-SPA/src/app/members/member-card/member-card.component.ts
+++ DatingApp-SPA/src/app/members/member-card/member-card.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { User } from 'src/app/_model/User';
+import { AuthService } from 'src/app/_services/auth.service';
+import { UserService } from 'src/app/_services/user.service';
+import { AlertifyService } from 'src/app/_services/alertify.service';
 
 @Component({
   selector: 'app-member-card',
@@ -8,9 +11,22 @@ import { User } from 'src/app/_model/User';
 })
 export class MemberCardComponent implements OnInit {
   @Input() user: User;
-  constructor() { }
+  constructor(private authServ: AuthService, private userServ: UserService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
+
+  sendLike(recipientId: number) {
+    const userId = this.authServ.decodedToken.nameid;
+
+    this.userServ.sendLike(userId, recipientId).subscribe(
+      res => {
+        this.alertify.success("You have liked: " + this.user.knownAs);
+      }, error => {
+        this.alertify.error(error);
+      }
+    );
+  };
+
 
 }
