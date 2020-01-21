bfb6425a8306814a09f86430109ac0dc82e5d62d
Erayus <raymondhieutran@gmail.com>
2020-01-21 18:36:55 +1100

    [New] Adding Delete functionality to the SPA

--- DatingApp-SPA/src/app/messages/messages.component.ts
+++ DatingApp-SPA/src/app/messages/messages.component.ts
@@ -40,6 +40,17 @@ export class MessagesComponent implements OnInit {
       });
   }
 
+  deleteMessage(id: number) {
+    this.alertify.confirm('Are you sure you want to delete this message?', () => {
+      this.userServ.deleteMessage(id, this.authService.decodedToken.nameid).subscribe(() => {
+        this.messages.splice(this.messages.findIndex(m => m.id === id), 1);
+        this.alertify.success("Message has been deleted successfully");
+      }, err => {
+        this.alertify.error('Failed to delete message');
+      });
+    })
+  }
+
   pageChanged(event: any) : void {
     this.pagination.currentPage = event.page;
     this.loadMessages();
